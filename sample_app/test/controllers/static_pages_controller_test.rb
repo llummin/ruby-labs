dc530f509bd4bee3e60b1fa2da92877f26dccbbb
maxim <maxim.elushev@yandex.ru>
2023-11-10 01:33:10 +0300

    writing tests and fixing problems

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,19 @@
-require "test_helper"
+require 'test_helper'
 
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+class StaticPagesControllerTest < ActionController::TestCase
+
   test "should get home" do
-    get static_pages_home_url
+    get :home
     assert_response :success
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get :help
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
     assert_response :success
   end
 end
