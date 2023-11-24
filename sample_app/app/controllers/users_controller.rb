15dbe2f54ea8c4b6ff11e659ac962f7c41672cca
maxim <maxim.elushev@yandex.ru>
2023-11-24 10:02:34 +0300

    finish user signup

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -9,9 +9,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)   # Не окончательная реализация!
+    @user = User.new(user_params)
     if @user.save
-      # Обработать успешное сохранение.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
